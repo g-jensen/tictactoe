0127ab41167ea58ad403b0ca7019c66694d42f9b
g-jensen <greg.micah.jensen@gmail.com>
2023-05-31 17:34:40 -0400

    Wrote first iteration of TicTacToe

--- spec/tictactoe/core_spec.clj
+++ spec/tictactoe/core_spec.clj
@@ -2,6 +2,61 @@
   (:require [speclj.core :refer :all]
             [tictactoe.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(def first-move-board (vec (cons \x (repeat 8 empty-tile))))
+
+(describe "A TicTacToe Game"
+
+  (it "initializes an empty 3x3 board"
+    (should= (repeat 9 empty-tile) empty-board))
+
+  (it "converts a board to a string"
+    (should= "_ _ _\n_ _ _\n_ _ _" (board->str empty-board))
+    (should= "x _ _\n_ _ _\n_ _ _" (board->str first-move-board)))
+
+  (it "displays a board"
+    (should= "_ _ _\n_ _ _\n_ _ _\n"
+             (with-out-str (display-board empty-board)))
+    (should= "x _ _\n_ _ _\n_ _ _\n"
+             (with-out-str (display-board first-move-board))))
+
+  (it "gets whose move it is"
+    (should= \x (player-to-move empty-board))
+    (should= \o (player-to-move first-move-board))
+    (should= \x (player-to-move [\x \o \_ \_ \_ \_ \_ \_ \_])))
+
+  (it "checks if a move is valid"
+    (should (move-valid? empty-board 0))
+    (should-not (move-valid? empty-board -1))
+    (should-not (move-valid? empty-board 9))
+    (should-not (move-valid? first-move-board 0)))
+
+  (it "plays a move on a board"
+    (should= empty-board (play-move empty-board -1))
+    (should= empty-board (play-move empty-board 9))
+    (should= first-move-board (play-move first-move-board 0))
+    (should= first-move-board (play-move empty-board 0))
+    (should= [\x \o \_ \_ \_ \_ \_ \_ \_] (play-move first-move-board 1)))
+
+  (it "gets the rows of a board"
+    (should= [[:a :b :c] [:d :e :f] [:g :h :i]]
+             (rows [:a :b :c :d :e :f :g :h :i])))
+
+  (it "gets the columns of a board"
+    (should= [[:a :d :g] [:b :e :h] [:c :f :i]]
+             (columns [:a :b :c :d :e :f :g :h :i])))
+
+  (it "gets the diagonals of a board"
+    (should= [[:a :e :i] [:c :e :g]]
+             (diagonals [:a :b :c :d :e :f :g :h :i])))
+
+  (it "checks if a player has won"
+    (should-not (win? empty-board))
+    (should (win? [\x \x \x \_ \_ \_ \_ \_ \_]))
+    (should (win? [\_ \_ \_ \x \x \x \_ \_ \_]))
+    (should (win? [\_ \_ \_ \_ \_ \_ \x \x \x]))
+    (should-not (win? [\x \x \_ \x \_ \_ \_ \_ \_]))
+    (should (win? [\x \_ \_ \x \_ \_ \x \_ \_]))
+    (should (win? [\_ \x \_ \_ \x \_ \_ \x \_]))
+    (should (win? [\_ \_ \x \_ \_ \x \_ \_ \x]))
+    (should (win? [\x \_ \_ \_ \x \_ \_ \_ \x]))
+    (should (win? [\_ \_ \x \_ \x \_ \x \_ \_]))))
--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,5 +1,79 @@
-(ns tictactoe.core)
+(ns tictactoe.core
+  (:require [clojure.string :as str]))
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(def empty-tile \_)
+(def empty-board (vec (repeat 9 empty-tile)))
+
+(defn board->str [board]
+  (->> (partition 3 board)
+       (map #(str/join " " %))
+       (str/join "\n")))
+
+(defn display-board [board]
+  (println (board->str board)))
+
+(defn in-range? [start end n]
+  (and (>= n start) (< n end)))
+
+(defn tile-count [board tile]
+  (count (filter #(= tile %) board)))
+
+(defn player-to-move [board]
+  (if (= (tile-count board \x) (tile-count board \o))
+    \x
+    \o))
+
+(defn move-valid? [board index]
+  (and (in-range? 0 9 index)
+       (= (nth board index) empty-tile)))
+
+(defn play-move [board index]
+  (if-not (move-valid? board index)
+    board
+    (assoc board index (player-to-move board))))
+
+(defn winning-line? [seq]
+  (and (not= empty-tile (first seq))
+       (apply = seq)))
+
+(defn rows [board]
+  (partition 3 board))
+
+(defn columns [board]
+  (for [i (range 0 3)]
+    (map #(nth % i) (rows board))))
+
+(defn diagonals [board]
+  [(map #(nth board (* 4 %)) (range 0 3))
+   (map #(nth board (* 2 (inc %))) (range 0 3))])
+
+(defn horizontal-win? [board]
+  (boolean (some true? (map winning-line? (rows board)))))
+
+(defn vertical-win? [board]
+  (boolean (some true? (map winning-line? (columns board)))))
+
+(defn diagonal-win? [board]
+  (boolean (some true? (map winning-line? (diagonals board)))))
+
+(defn win? [board]
+  (or (horizontal-win? board)
+      (vertical-win? board)
+      (diagonal-win? board)))
+
+(defn winner [board]
+  (if (= \x (player-to-move board)) \o \x))
+
+(defn display-winning-message [board]
+  (println (str (winner board) " has won!")))
+
+(defn get-user-move []
+  (Integer/parseInt (read-line)))
+
+;;TODO - test main
+(defn -main [& args]
+  (loop [board empty-board]
+    (display-board board)
+    (if (win? board)
+      (display-winning-message board)
+      (recur (play-move board (get-user-move))))))
