44a6e1bd4bd65f76ad22d7636c1aefe8b4ba1473
g-jensen <greg.micah.jensen@gmail.com>
2023-06-01 16:46:32 -0400

    Wrapped ui tests in 'describe'

--- spec/tictactoe/ui_spec.clj
+++ spec/tictactoe/ui_spec.clj
@@ -3,12 +3,13 @@
             [tictactoe.board :refer :all]
             [tictactoe.ui :refer :all]))
 
-(it "converts a board to a string"
-  (should= "_ _ _\n_ _ _\n_ _ _" (board->str empty-board))
-  (should= "x _ _\n_ _ _\n_ _ _" (board->str first-move-board)))
+(describe "A TicTacToe Console UI"
+  (it "converts a board to a string"
+    (should= "_ _ _\n_ _ _\n_ _ _" (board->str empty-board))
+    (should= "x _ _\n_ _ _\n_ _ _" (board->str first-move-board)))
 
-(it "displays a board"
-  (should= "_ _ _\n_ _ _\n_ _ _\n"
-           (with-out-str (display-board empty-board)))
-  (should= "x _ _\n_ _ _\n_ _ _\n"
-           (with-out-str (display-board first-move-board))))
+  (it "displays a board"
+    (should= "_ _ _\n_ _ _\n_ _ _\n"
+             (with-out-str (display-board empty-board)))
+    (should= "x _ _\n_ _ _\n_ _ _\n"
+             (with-out-str (display-board first-move-board)))))
