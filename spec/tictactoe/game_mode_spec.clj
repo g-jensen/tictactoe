f39463cb7e3cb7537d03c1eafcb58980f34cf014
g-jensen <greg.micah.jensen@gmail.com>
2023-06-13 12:56:53 -0400

    Added tests for user input aspects of PvPGame and PvCGame

--- spec/tictactoe/game_mode_spec.clj
+++ spec/tictactoe/game_mode_spec.clj
@@ -1,12 +1,39 @@
 (ns tictactoe.game-mode-spec
   (:require [speclj.core :refer :all]
             [tictactoe.game-mode :refer :all]
-            [tictactoe.board :as board]
+            [tictactoe.utils :as utils]
             [tictactoe.move :as move]))
 
-;; TODO - test next-board with PvPGame
 (describe "A TicTacToe Game-mode"
-  (it "gets the next board"
-    ;; TODO - ask if I should put a vector literal here or if this is good
-    (should= (move/play-move board/empty-board (move/get-computer-move board/empty-board))
-             (next-board (->PvCGame) board/empty-board))))
+
+  (with-stubs)
+  (context "PvPGame"
+
+    (it "gets the initial board"
+      (should= utils/empty-board (initial-board (->PvPGame))))
+
+    (it "gets the next board"
+      (with-redefs [read-line (stub :read-line {:return "1"})]
+        (should= [\x \_ \_ \_ \_ \_ \_ \_ \_]
+                 (next-board (->PvPGame) utils/empty-board))
+        (should-have-invoked :read-line))
+
+      (with-redefs [read-line (stub :read-line {:return "2"})]
+        (should= [\x \o \_ \_ \_ \_ \_ \_ \_]
+                 (next-board (->PvPGame) [\x \_ \_ \_ \_ \_ \_ \_ \_]))
+        (should-have-invoked :read-line))))
+
+  (context "PvCGame"
+    (it "gets the initial board"
+      (should= utils/empty-board (initial-board (->PvCGame \x)))
+      (should= [\_ \_ \_ \_ \_ \_ \_ \_ \x] (initial-board (->PvCGame \o))))
+
+    (it "gets the next board"
+      (with-redefs [read-line (stub :read-line {:return "1"})]
+        (should= [\x \_ \_ \_ \_ \_ \_ \_ \_]
+                 (next-board (->PvCGame \x) utils/empty-board))
+        (should-have-invoked :read-line))
+
+      ;; TODO - ask if I should put a vector literal here or if this is good
+      (should= (move/play-move utils/empty-board (move/get-computer-move utils/empty-board))
+               (next-board (->PvCGame \o) utils/empty-board)))))
