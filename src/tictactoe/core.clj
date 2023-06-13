21b0d38aff2d3aa7afded19dc0474f6dd9ac080a
g-jensen <greg.micah.jensen@gmail.com>
2023-06-13 13:47:41 -0400

    Created choice for going first or second

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,14 +1,13 @@
 (ns tictactoe.core
   (:require
-    [tictactoe.board :as board]
     [tictactoe.ui :as ui]
     [tictactoe.game-state :as game-state]
     [tictactoe.game-mode :as game-mode]))
 
 (defn -main [& args]
-  (ui/display-game-modes-prompt game-mode/game-modes)
-  (let [game-mode (ui/prompt-game-mode game-mode/game-modes)]
-    (loop [board board/empty-board]
+  (let [game-mode (ui/evaluate-menu ui/game-mode-menu)]
+    (ui/display-guide)
+    (loop [board (game-mode/initial-board game-mode)]
       (ui/display-board board)
       (if (game-state/game-over? board)
         (ui/display-game-over-message board)
