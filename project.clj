b6dedad5a10a0ead85c349edff61f812cad2d833
g-jensen <greg.micah.jensen@gmail.com>
2023-06-26 14:30:10 -0400

    Imported Quil

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :main tictactoe.core
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/java.jdbc "0.7.8"]
-                 [org.xerial/sqlite-jdbc "3.23.1"]]
+                 [org.xerial/sqlite-jdbc "3.23.1"]
+                 [quil "3.1.0"]]
   :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
   :plugins [[speclj "3.3.2"]]
   :test-paths ["spec"])
