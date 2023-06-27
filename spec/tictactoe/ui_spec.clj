69e53a4d7079b8870521bca6580a819d3d596cd8
g-jensen <greg.micah.jensen@gmail.com>
2023-06-26 22:48:26 -0400

    Added new line in game over message

--- spec/tictactoe/ui_spec.clj
+++ spec/tictactoe/ui_spec.clj
@@ -13,11 +13,11 @@
              (with-out-str (print-board (utils-spec/first-move-board 3)))))
 
   (it "prints the game over message"
-    (should= "x has won!\nPlay Again:\n" (with-out-str (print-game-over-message
+    (should= "x has won!\n\nPlay Again:\n" (with-out-str (print-game-over-message
                                         [\x \x \x \o \o \_ \_ \_ \_])))
-    (should= "o has won!\nPlay Again:\n" (with-out-str (print-game-over-message
+    (should= "o has won!\n\nPlay Again:\n" (with-out-str (print-game-over-message
                                             [\o \o \o \x \x \_ \x \_ \_])))
-    (should= "x has won!\nPlay Again:\n" (with-out-str (print-game-over-message
+    (should= "x has won!\n\nPlay Again:\n" (with-out-str (print-game-over-message
                                             [\o \o \_ \x \x \x \_ \_ \_])))
-    (should= "tie!\nPlay Again:\n" (with-out-str (print-game-over-message
+    (should= "tie!\n\nAdPlay Again:\n" (with-out-str (print-game-over-message
                                     [\o \x \x \x \o \o \o \x \x])))))
