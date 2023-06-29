f6e190cb65802c0155d6b9e8f60de992c667e4da
g-jensen <greg.micah.jensen@gmail.com>
2023-06-29 09:30:50 -0400

    Moved run-tictactoe into game-state

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,22 +1,13 @@
 (ns tictactoe.core
   (:gen-class)
-  (:require [tictactoe.game-state :as game-state]
-            [tictactoe.ui :as ui]
-            [tictactoe.menu :as menu]))
-
-(defmulti run-tictactoe identity)
-
-(defmethod run-tictactoe :console [_]
-  (ui/console-initialize)
-  (loop [state (game-state/initial-state)]
-    (ui/console-draw state)
-    (if (game-state/over? state)
-      (do (game-state/clean-up state)
-          (recur (game-state/initial-state)))
-      (recur (game-state/update-state state)))))
-
-(defmethod run-tictactoe :quil [_]
-  (ui/quil-initialize))
+  (:require [clojure.string :as str]
+            [tictactoe.menu :as menu]
+            [tictactoe.game-state :as game-state]
+            [tictactoe.console-game]
+            [tictactoe.quil-game]))
 
 (defn -main [& args]
-  (run-tictactoe (menu/evaluate-menu menu/ui-picker-menu)))
+  (let [state {:state :ui}]
+    (println (:label (menu/ui-components state)))
+    (println (str/join "\n" (:options (menu/ui-components state))))
+    (game-state/run-tictactoe (menu/next-state state (read-line)))))
