ff060b6a9161dfbf5c9a2dd56ab8af4ee227445d
g-jensen <greg.micah.jensen@gmail.com>
2023-06-29 09:29:34 -0400

    Refactored ui into quil-game. Still needs works

---
+++ spec/tictactoe/quil_game_spec.clj
@@ -0,0 +1,5 @@
+(ns tictactoe.quil-game-spec
+  (:require [speclj.core :refer :all]
+            [tictactoe.game-state :refer :all]))
+
+(describe "A Quil TicTacToe Game")
---
+++ src/tictactoe/quil_game.clj
@@ -0,0 +1,142 @@
+(ns tictactoe.quil-game
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [tictactoe.board-state :as board-state]
+            [tictactoe.database :as database]
+            [tictactoe.game-mode :as game-mode]
+            [tictactoe.game-state :as game-state]
+            [tictactoe.menu :as menu]
+            [tictactoe.move :as move]
+            [tictactoe.utils :as utils])
+  (:import (tictactoe.game_mode PvCGame)))
+
+(defn point-in-rect? [point rect]
+  (let [[x1 y1] point
+        [x2 y2 width height] rect]
+    (and (>= x1 x2)
+         (<= x1 (+ x2 width))
+         (>= (+ y1 height) y2)
+         (<= (+ y1 height) (+ y2 height)))))
+
+(defn quil-button [[x y] [width height] text]
+  (q/fill 255)
+  (q/rect x (- y height) width height)
+  (q/fill 0)
+  (q/text text x y))
+
+(defn quil-setup []
+  (q/frame-rate 30)
+  (q/background 200)
+  (menu/next-state {:state :done} nil))
+
+(defn board-index-to-coords [index size]
+  [(* 50 (inc (quot index size))) (+ 50 (* 50 (inc (mod index size))))])
+
+(defn quil-draw-board [board]
+  (let [size (board-state/board-size board)]
+    (doall (for [i (range 0 (count board))
+                 :let [x (mod i size)
+                       y (quot i size)]]
+             (quil-button (board-index-to-coords i size) [50 50] (str (nth board (+ (* size y) x))))))))
+
+(defn quil-draw-buttons [state]
+  (let [components (menu/ui-components state)
+        label (:label components)
+        options (:options components)]
+    (do (q/text label 5 20)
+        (doall
+          (for [i (range 0 (count options))]
+            (quil-button
+              [5 (* 20 (+ i 3))]
+              [(* 15 (count (nth options i))) 20]
+              (nth options i)))))))
+
+(defn picked-option [mouse-pos options]
+  (first (filter #(point-in-rect? mouse-pos [5 (* 20 (+ % 3)) 150 20])
+                 (range 0 (count options)))))
+
+(defn picked-tile [mouse-pos board]
+  (first (filter #(point-in-rect? mouse-pos (concat (board-index-to-coords % (board-state/board-size board)) [50 50]))
+                 (range 0 (count board)))))
+
+(defn click-button [state data]
+  (let [options (:options (menu/ui-components state))
+        choice (picked-option [(:x data) (:y data)] options)]
+    (if (nil? choice)
+      state
+      (menu/next-state state (str (inc choice))))))
+
+(defn click-tile [state data]
+  (let [tile (picked-tile [(:x data) (:y data)] (:board state))
+        database (:database state)
+        date (:date state)
+        old-date (:old-date state)
+        board (:board state)
+        gamemode (:gamemode state)]
+    (if (nil? tile)
+      state
+      (do (database/delete-game database old-date)
+          (database/update-game database date board gamemode)
+          (assoc state :board (move/play-move (:board state) tile))))))
+
+(defn computer-turn? [state]
+  (and (not (empty? (utils/empty-indices (:board state))))
+       (instance? PvCGame (:gamemode state))
+       (not= (move/player-to-move (:init-board (:gamemode state)))
+             (move/player-to-move (:board state)))))
+
+(defn init-gamemode [state]
+  (let [size (:board-size state)
+        difficulty (:difficulty state)]
+    (if (= :pvp (:versus-type state))
+      (game-mode/->PvPGame size (:board state))
+      (game-mode/->PvCGame size (:board state) difficulty))))
+
+(defn quil-draw [state]
+  (q/fill 200)
+  (q/rect 0 0 384 384)
+  (q/fill 0)
+  (q/text-size 20)
+  (if (and (not= :done (:state state)))
+    (quil-draw-buttons state)
+    (quil-draw-board (:board state)))
+  (if (:over? state)
+    (quil-button [50 300] [120 50] "Play Again?")))
+
+(defn quil-update [state]
+  (if (= :done (:state state))
+    (if (not (:gamemode state))
+      (assoc state :gamemode (init-gamemode state))
+      (let [database (:database state)
+            date (:date state)
+            old-date (:old-date state)
+            gamemode (:gamemode state)
+            board (:board state)]
+        (if (computer-turn? state)
+          (do (database/delete-game database old-date)
+              (database/update-game database date board gamemode)
+              (assoc state :board (game-mode/next-board gamemode board)))
+          (if (board-state/game-over? board)
+            (do (database/delete-game database date)
+                (assoc state :over? true))
+            state))))
+    state))
+
+(defn quil-mouse-clicked [state data]
+  (if-not (= :done (:state state))
+    (click-button state data)
+    (let [play-again? (point-in-rect? [(:x data) (:y data)] [50 300 120 50])]
+      (if (and (:over? state) play-again?)
+        (quil-setup)
+        (click-tile state data)))))
+
+(defmethod game-state/run-tictactoe :quil [_]
+  (q/defsketch tictactoe
+               :title "TicTacToe Game"
+               :settings #(q/smooth 2)
+               :setup quil-setup
+               :draw quil-draw
+               :update quil-update
+               :mouse-clicked quil-mouse-clicked
+               :size [384 384]
+               :middleware [m/fun-mode]))
