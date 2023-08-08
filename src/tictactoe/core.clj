47f6e1d50d209f5e537b1768a1454351ea878d96
g-jensen <greg.micah.jensen@gmail.com>
2023-08-08 09:41:59 -0400

    Integrated web-game

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -2,7 +2,8 @@
   (:gen-class)
   (:require [tictactoe.game-state :as game-state]
             [tictactoe.console-game]
-            [tictactoe.quil-game]))
+            [tictactoe.quil-game]
+            [tictactoe.web-game]))
 
 (defn -main [& [ui]]
   (game-state/run-tictactoe {:ui (keyword ui)}))
