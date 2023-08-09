cb79c76ce29c476cba2028f90b25e7e4c5c34fc9
g-jensen <greg.micah.jensen@gmail.com>
2023-08-09 17:11:05 -0400

    Updated quil_game to use new next-state

--- spec/tictactoe/quil_game_spec.clj
+++ spec/tictactoe/quil_game_spec.clj
@@ -12,6 +12,20 @@
     (should (point-in-rect? [0 -1] [0 0 1 1]))
     (should (point-in-rect? [1 -1] [0 0 1 1])))
 
+  (it "adds points"
+    (should= [0 0] (add-points [0 0] [0 0]))
+    (should= [1 0] (add-points [1 0] [0 0]))
+    (should= [1 0] (add-points [0 0] [1 0]))
+    (should= [0 1] (add-points [0 0] [0 1]))
+    (should= [4 6] (add-points [1 2] [3 4])))
+
+  (it "subtracts points"
+    (should= [0 0] (sub-points [0 0] [0 0]))
+    (should= [1 0] (sub-points [1 0] [0 0]))
+    (should= [-1 0] (sub-points [0 0] [1 0]))
+    (should= [0 -1] (sub-points [0 0] [0 1]))
+    (should= [-2 -2] (sub-points [1 2] [3 4])))
+
   (it "converts a board index to coordinates"
     (should= [50 100] (board-index-to-coords 0 20 50))
     (should= [50 100] (board-index-to-coords 0 10 50))
@@ -19,16 +33,45 @@
     (should= [50 200] (board-index-to-coords 2 20 50))
     (should= [50 250] (board-index-to-coords 3 10 50)))
 
-  (it "clicks a button"
-    (should= :database (:state (update-ui {:state :database} {:x 0 :y 0})))
-    (should= :load-type (:state (update-ui {:state :database} {:x 5 :y 60})))
-    (should= :load-type (:state (update-ui {:state :database} {:x 5 :y 80})))
-    (should= :database (:state (update-ui {:state :database} {:x 5 :y 100}))))
+  (context "updates the ui state"
 
-  (it "clicks a tile on a board"
-    (should= (utils/empty-board 3)
-             (:board (click-tile {:board (utils/empty-board 3)} {:x 0 :y 0})))
-    (should= [\x \_ \_ \_ \_ \_ \_ \_ \_]
-             (:board (click-tile {:board (utils/empty-board 3)} {:x 50 :y 100})))
-    (should= [\_ \x \_ \_ \_ \_ \_ \_ \_]
-             (:board (click-tile {:board (utils/empty-board 3)} {:x 50 :y 150})))))
+    (it "for a counter"
+      (should= {:state :board-size, :counter-val 3}
+               (update-ui {:state :board-size} {:x 0 :y 0}))
+      (should= {:state :versus-type, :counter-val 3, :board-size 3, :board [\_ \_ \_ \_ \_ \_ \_ \_ \_]}
+               (update-ui {:state :board-size, :counter-val 3} {:x 5 :y 85}))
+      (should= {:state :board-size, :counter-val 4}
+               (update-ui {:state :board-size, :counter-val 3} {:x 36 :y 59}))
+      (should= {:state :board-size, :counter-val 3}
+               (update-ui {:state :board-size, :counter-val 3} {:x 6 :y 59}))
+      (should= {:state :board-size, :counter-val 3}
+               (update-ui {:state :board-size, :counter-val 4} {:x 6 :y 59})))
+
+    (it "for a menu"
+      (should= {:state :database}
+               (update-ui {:state :database} {:x 0 :y 0}))
+      (should= {:state :database}
+               (update-ui {:state :database} {:x 5 :y 85})))
+
+    (it "for a board"
+      (should= {:state :done}
+               (update-ui {:state :done} {:x 0 :y 0}))
+      (should= {:state :done}
+               (update-ui {:state :done} {:x 36 :y 59}))))
+
+  (context "gets the input of a click"
+
+    (it "for a menu"
+      (should= "1" (get-input {:state :database} {:x 5 :y 40}))
+      (should= "2" (get-input {:state :database} {:x 5 :y 61}))
+      (should-be-nil (get-input {:state :database} {:x 0 :y 0})))
+
+    (it "for a counters"
+      (should= "3" (get-input {:state :board-size :counter-val 3} {:x 5 :y 85}))
+      (should-be-nil (get-input {:state :board-size :counter-val 3} {:x 0 :y 0})))
+
+    (it "for a board"
+      (should= "1" (get-input {:state :done :board (utils/empty-board 3)} {:x 50 :y 50}))
+      (should= "2" (get-input {:state :done :board (utils/empty-board 3)} {:x 50 :y 101}))
+      (should= "4" (get-input {:state :done :board (utils/empty-board 3)} {:x 101 :y 50}))
+      (should-be-nil (get-input {:state :done} {:x 0 :y 0})))))
--- src/tictactoe/quil_game.clj
+++ src/tictactoe/quil_game.clj
@@ -3,11 +3,11 @@
             [quil.middleware :as m]
             [tictactoe.board-state :as board-state]
             [tictactoe.game-state :as gs]
-            [tictactoe.move :as move]
             [tictactoe.utils :as utils]))
 
 (defmulti draw-components #(:type (gs/ui-components %)))
 (defmulti update-ui (fn [state _] (:type (gs/ui-components state))))
+(defmulti get-input (fn [state _] (:type (gs/ui-components state))))
 
 (defn point-in-rect? [point rect]
   (let [[x1 y1] point
@@ -16,17 +16,6 @@
          (<= x1 (+ x2 width))
          (>= (+ y1 height) y2)
          (<= (+ y1 height) (+ y2 height)))))
-
-(defn quil-button [[x y] [width height] text]
-  (q/fill 255)
-  (q/rect x (- y height) width height)
-  (q/fill 0)
-  (q/text text x y))
-
-(defn quil-setup [state]
-  (q/frame-rate 30)
-  (q/background 200)
-  (gs/next-state state nil))
 
 (defn add-points [[x1 y1] [x2 y2]]
   [(+ x1 x2) (+ y1 y2)])
@@ -37,29 +26,26 @@
 (defn board-index-to-coords [index size tile-size]
   [(* tile-size (inc (quot index size))) (+ tile-size (* tile-size (inc (mod index size))))])
 
-(defn draw-tiles [board pos tile-size]
-  (let [board-size (board-state/board-size board)]
-    (for [i (range 0 (count board))
-          :let [x (mod i board-size)
-                y (quot i board-size)]]
-      (quil-button (add-points pos (board-index-to-coords i board-size tile-size)) [tile-size tile-size] (str (nth board (+ (* board-size y) x)))))))
+(defn- clicked-tiles [mouse-pos board tile-size]
+  (let [size (board-state/board-size board)]
+    (filter #(point-in-rect? mouse-pos
+                             (concat (board-index-to-coords % size tile-size)
+                                     [tile-size tile-size]))
+            (range 0 (count board)))))
 
-(defn draw-board [board]
-  (if (utils/board-3d? board)
-    (doall (for [i (range 0 (count board))]
-             (doall (draw-tiles (nth board i) [0 (* i 90)] 30))))
-    (doall (draw-tiles board [0 0] 50))))
+(defn- updated-value [state data]
+  (let [valid? (:valid? (gs/ui-components state))
+        init (:initial-value (gs/ui-components state))
+        val (get state :counter-val init)]
+    (cond
+      (point-in-rect? [(:x data) (:y data)] [5 85 30 30])
+      (if (valid? (dec val)) (dec val) val)
+      (point-in-rect? [(:x data) (:y data)] [35 85 30 30])
+      (if (valid? (inc val)) (inc val) val)
+      :else
+      val)))
 
-(defn picked-option [mouse-pos options]
-  (first (filter #(point-in-rect? mouse-pos [5 (* 20 (+ % 3)) 150 20])
-                 (range 0 (count options)))))
-
-(defn clicked-tiles [mouse-pos board tile-size]
-  (filter #(point-in-rect? mouse-pos
-                           (concat (board-index-to-coords % (board-state/board-size board) tile-size) [tile-size tile-size]))
-          (range 0 (count board))))
-
-(defn picked-tile [mouse-pos board]
+(defn- picked-tile [mouse-pos board]
   (if (utils/board-3d? board)
     (first (apply concat (for [i (range 0 (count board))]
                            (map #(+ (* i 9) %)
@@ -67,46 +53,38 @@
                                                (nth board i) 30)))))
     (first (clicked-tiles mouse-pos board 50))))
 
-(defn click-tile [state data]
-  (let [tile (picked-tile [(:x data) (:y data)] (:board state))]
-    (if (nil? tile)
-      state
-      (assoc state :board (move/play-move (:board state) tile)))))
-
-(defn quil-draw [state]
-  (q/fill 200)
-  (q/rect 0 0 384 384)
+(defn- quil-button [[x y] [width height] text]
+  (q/fill 255)
+  (q/rect x (- y height) width height)
   (q/fill 0)
-  (q/text-size 20)
-  (if (not= :done (:state state))
-    (draw-components state)
-    (draw-board (:board state)))
-  (if (:over? state)
-    (quil-button [150 300] [120 50] "Play Again?")))
+  (q/text text x y))
 
-(defn mouse-clicked [state data]
-  (if-not (= :done (:state state))
-    (update-ui state data)
-    (let [play-again? (point-in-rect? [(:x data) (:y data)] [150 300 120 50])
-          state (click-tile state data)]
-      (if (and (:over? state) play-again?)
-        (do (gs/next-state {:ui :quil} nil))
-        (do (gs/db-save-game (gs/next-board state))
-            state)))))
+(defn- draw-tiles [board pos tile-size]
+  (let [board-size (board-state/board-size board)]
+    (for [i (range 0 (count board))
+          :let [x (mod i board-size)
+                y (quot i board-size)]]
+      (quil-button (add-points pos (board-index-to-coords i board-size tile-size))
+                   [tile-size tile-size] (str (nth board (+ (* board-size y) x)))))))
 
-(defmethod gs/next-board :quil [state]
-  (let [board (:board state)
-        difficulty (:difficulty state)]
-    (if (gs/computer-turn? state)
-      (assoc state :board (move/play-move board (move/get-computer-move difficulty board)))
-      state)))
+(defmethod get-input :menu [state data]
+  (let [mouse-pos [(:x data) (:y data)]
+        options (:options (gs/ui-components state))
+        inputs (filter #(point-in-rect? mouse-pos [5 (* 20 (+ % 3)) 150 20])
+                      (range 0 (count options)))]
+    (if-not (empty? inputs)
+      (str (inc (first inputs))))))
 
-(defmethod update-ui :menu [state data]
-  (let [options (:options (gs/ui-components state))
-        choice (picked-option [(:x data) (:y data)] options)]
-    (if (nil? choice)
-      state
-      (gs/next-state state (str (inc choice))))))
+(defmethod get-input :counter [state data]
+  (let [mouse-pos [(:x data) (:y data)]]
+    (if (point-in-rect? mouse-pos [5 115 60 30])
+      (str (:counter-val state)))))
+
+(defmethod get-input :board [state data]
+  (let [mouse-pos [(:x data) (:y data)]
+        input (picked-tile mouse-pos (:board state))]
+    (if-not (nil? input)
+      (str (inc input)))))
 
 (defmethod draw-components :menu [state]
   (let [comps (gs/ui-components state)
@@ -115,22 +93,11 @@
     (do (q/text label 5 20)
         (doall
           (for [i (range 0 (count options))]
-            (quil-button
-              [5 (* 20 (+ i 3))]
-              [(* 15 (count (nth options i))) 20]
-              (nth options i)))))))
+            (quil-button [5 (* 20 (+ i 3))]
+                         [(* 15 (count (nth options i))) 20]
+                         (nth options i)))))))
 
-(defn updated-value [state data]
-  (let [valid? (:valid? (gs/ui-components state))
-        init (:initial-value (gs/ui-components state))
-        val (get state :counter-val init)]
-    (cond
-      (point-in-rect? [(:x data) (:y data)] [5 85 30 30])
-        (if (valid? (dec val)) (dec val) val)
-      (point-in-rect? [(:x data) (:y data)] [35 85 30 30])
-        (if (valid? (inc val)) (inc val) val)
-      :else
-        val)))
+(defmethod update-ui :default [state _] state)
 
 (defmethod update-ui :counter [state data]
   (if (point-in-rect? [(:x data) (:y data)] [5 115 60 30])
@@ -148,13 +115,40 @@
         (quil-button [35 85] [30 30] "+")
         (quil-button [5 115] [60 30] "done"))))
 
-(defmethod gs/run-tictactoe :quil [state]
+(defmethod draw-components :board [state]
+  (let [board (:board state)]
+    (if (utils/board-3d? board)
+      (doall (for [i (range 0 (count board))]
+               (doall (draw-tiles (nth board i) [0 (* i 90)] 30))))
+      (doall (draw-tiles board [0 0] 50))))
+  (if (:over? state)
+    (quil-button [150 300] [120 50] "Play Again?")))
+
+(defn- quil-setup []
+  (q/frame-rate 30)
+  (q/background 200)
+  (gs/next-state {} nil))
+
+(defn- quil-draw [state]
+  (q/fill 200)
+  (q/rect 0 0 384 384)
+  (q/fill 0)
+  (q/text-size 20)
+  (draw-components state))
+
+(defn- mouse-clicked [state data]
+  (println data)
+  (if (:over? state)
+    (gs/next-state {} nil)
+    (gs/next-state (update-ui state data)
+                   (get-input state data))))
+
+(defmethod gs/run-tictactoe :quil [_]
   (q/defsketch tictactoe
                :title "TicTacToe Game"
                :settings #(q/smooth 2)
-               :setup #(quil-setup state)
+               :setup quil-setup
                :draw quil-draw
-               :update gs/update-state
                :mouse-clicked mouse-clicked
                :size [384 384]
                :middleware [m/fun-mode]))
